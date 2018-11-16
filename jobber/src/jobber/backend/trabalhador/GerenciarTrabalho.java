191db35780af619024c42adb1e8c4f4c9fa2e21a
Guihgo <guihgo100milha@gmail.com>
2018-11-16 02:31:29 -0200

    listar trabalhos

--- jobber/src/jobber/backend/trabalhador/GerenciarTrabalho.java
+++ jobber/src/jobber/backend/trabalhador/GerenciarTrabalho.java
@@ -5,8 +5,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import jobber.backend.Conexao;
+import jobber.modelo.Conta;
 import jobber.modelo.Trabalho;
 
 public class GerenciarTrabalho extends Conexao{
@@ -20,31 +22,29 @@ public class GerenciarTrabalho extends Conexao{
         this.conexao = conexao;
     }
 
-    public Trabalho[] listar() {
-        /*
-        jobber.modelo.Conta conta = new jobber.modelo.Conta();
+    public ArrayList<Trabalho> listar(Conta conta) {
+        ArrayList<Trabalho> trabalhos = new ArrayList<Trabalho>();
+
         try {
-//            ps = this.conexao.getConnection().prepareStatement("SELECT * FROM conta WHERE conta_email=? AND conta_senha=? LIMIT 1");
-//            ps.setString(1, email);
-//            ps.setString(2, password);
-//            rs = ps.executeQuery();
-//            if(rs.last()) {
+            ps = this.conexao.getConnection().prepareStatement("SELECT trabalho.* FROM trabalho NATURAL JOIN conta;");
+            rs = ps.executeQuery();
+            showTableResultSet(rs);
+            while(rs.next()) {
+
+                System.out.println("rs.getString(): "+rs.getString("trabalho_nome"));
 //                conta.setLogado(true);
 //                conta.setId(rs.getInt("conta_id"));
 //                conta.setEmail(rs.getString("conta_email"));
 //                conta.setNome(rs.getString("conta_nome"));
 //                conta.setTipo(rs.getInt("conta_tipo"));
-//            } else {
-//                conta.setLogado(false);
-//            }
+
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
             closeStatement(stmt, rs);
-            return conta;
+            return trabalhos;
         }
-        */
-        return null;
     }
 
     public void adicionar() {
